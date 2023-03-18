77c81f31d6a98f4758e1a42ee8da49bba7b6dfe9
neera <neerajrao04@gmail.com>
2023-03-19 00:18:58 +0530

    mini project

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,6 +6,7 @@ function BookList() {
        const {books} = useContext(booksContext);
     const List = books.map((book)=>{
         return <BookShow book={book}></BookShow>
+        
     })
     return (
       <div className="book-list">{List}</div>
